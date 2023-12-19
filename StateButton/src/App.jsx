d8fbc9e9cc962ad656daa666badca3b1728f5755
Pasang Tshering Sherpa <111878163+Pasangcrp@users.noreply.github.com>
2023-12-19 08:21:38 +0545

    Edited StateButton (19th Dec)

--- StateButton/src/App.jsx
+++ StateButton/src/App.jsx
@@ -1,22 +1,80 @@
+
+//ASSIGNMENT:
+
+// import { useState } from "react";
+
+
+
+// const App = () => {
+
+//   const [isClicked, setIsClicked] = useState(0);
+
+//   const handleClick = () => {
+//     setIsClicked(!isClicked);
+//   }
+
+//     return(
+//       <button onClick={handleClick}>
+// {isClicked ? 'Clicked': 'Click Me'}
+//       </button>
+//     );
+// }
+
+// export default App
+
+
+//ClassCOde 1 :
+// import { useState } from "react";
+
+
+// const App = () => {
+
+// const [counter, setCounter] = useState(0);
+
+// return (
+
+//   <div>
+//   <div>{counter}</div>
+//   <button onClick={() => setCounter(counter+1)}>
+//     plus
+//     </button>
+
+//   <button onClick={() => setCounter(0)}>
+//     zero
+//   </button>
+//   </div>
+//   )
+// }
+
+// export default App
+
+
+// CODE 2
 import { useState } from "react";
 
-const App = () => {
+const App = () =>{
+  const [ counter , setCounter ] = useState(0)
 
-  const [isClicked, setIsClicked] = useState(0);
+  const increaseByOne = () => setCounter(counter+1)
 
-  const handleClick = () => {
-    setIsClicked(!isClicked);
-  }
+  const decreaseByOne = () => setCounter(counter-1)
 
-    return(
-      <button onClick={handleClick}>
-{isClicked ? 'Clicked': 'Click Me'}
+  const setTOZero = () => setCounter(0)
+
+  return (
+    <div>
+      <div>{counter}</div>
+      <button onClick={increaseByOne}>
+        plus
       </button>
-    );
+      <button onClick={decreaseByOne}>
+        Minus
+      </button>
+      <button onClick={setTOZero}>
+        zero
+      </button>
+    </div>
+  )
 }
 
 export default App
-
-
-
-
